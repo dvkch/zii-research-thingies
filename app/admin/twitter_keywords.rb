9e1ff96afd4586beec066b64e54649be11ec979f
Stanislas Chevallier <contact@stanislaschevallier.fr>
2022-04-03 01:15:18 +0200

    Fix bug

--- app/admin/twitter_keywords.rb
+++ app/admin/twitter_keywords.rb
@@ -15,8 +15,8 @@ ActiveAdmin.register_page 'Twitter Keywords' do
         'Most common words are: '
       end
       scope = (selected_twitter_search&.twitter_tweets || TwitterTweet)
-      scope = scope.where('date >= ?', selected_from) if selected_from
-      scope = scope.where('date <= ?', selected_to) if selected_to
+      scope = scope.where('date >= ?', selected_from) if selected_from.present?
+      scope = scope.where('date <= ?', selected_to) if selected_to.present?
       words = scope.common_words(min_word_length: selected_min_character_count, limit: selected_limit).map(&:first)
       ul class: 'word_tags' do
         words.each do |w|
