e8a5430ba22168075a96b250f659e0c5ca6c9368
Stanislas Chevallier <contact@stanislaschevallier.fr>
2022-04-02 22:15:25 +0200

    Add common word listing

--- app/models/twitter_tweet.rb
+++ app/models/twitter_tweet.rb
@@ -8,6 +8,19 @@ class TwitterTweet < ApplicationRecord
     "https://twitter.com/#{username}/status/#{twitter_id}"
   end
 
+  def self.common_words(min_word_length: 2, limit: 100)
+    subquery = self.select("regexp_split_to_table(twitter_tweets.content, '\s') as word")
+    query = <<SQL
+SELECT word as w, count(*) AS c
+FROM (#{subquery.to_sql}) t
+WHERE length(word) >= #{min_word_length}
+GROUP BY w
+ORDER BY c DESC
+LIMIT #{limit}
+SQL
+    ActiveRecord::Base.connection.exec_query(query).to_a.map { |r| r.values_at('w', 'c') }
+  end
+
   include PgSearch::Model
   pg_search_scope :search,
                   against: :content,
--- spec/models/twitter_tweet_spec.rb
+++ spec/models/twitter_tweet_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe TwitterTweet, type: :model do
     it { should validate_presence_of(:content) }
     it { should validate_presence_of(:twitter_id) }
   end
+
+  describe 'common words' do
+    it 'generates a list of the most common words used in tweets' do
+      create(:twitter_search, allow_update: '1', twitter_search_sources: [
+        source1 = build(:twitter_search_source, query: 'test 1'),
+        source2 = build(:twitter_search_source, query: 'test 2')
+      ])
+      create(:twitter_tweet, twitter_search_source: source1, content: 'I am a rabbit')
+      create(:twitter_tweet, twitter_search_source: source2, content: 'I love my rabbit')
+      expect(TwitterTweet.common_words).to eq [['rabbit', 2], ['am', 1], ['my', 1], ['love', 1]]
+      expect(TwitterTweet.common_words(limit: 1)).to eq [['rabbit', 2]]
+      expect(TwitterTweet.common_words(min_word_length: 4)).to eq [['rabbit', 2], ['love', 1]]
+      expect(source1.twitter_tweets.common_words).to eq [['rabbit', 1], ['am', 1]]
+    end
+  end
 end
